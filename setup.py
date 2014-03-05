be7e836d08db7a114a359e098d79795d74926590
Charlie Denton <charlie@meshy.co.uk>
2014-03-05 13:07:23 +0000

    Bump version to 0.0.4

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='colour-runner',
-    version='0.0.3',
+    version='0.0.4',
     description='Colour formatting for unittest tests',
     url='https://github.com/meshy/colour-runner',
     author='Charlie Denton',
